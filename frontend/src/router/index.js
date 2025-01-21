a97e0c2905d607f6fd8a0df98a5c5c843f3edc74
ungit003 <ungit003@gamil.com>
2025-01-21 10:41:30 +0900

    feat: 새로운 프론트엔드

---
+++ frontend/src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // 여기에 라우트를 정의합니다
+  ]
+})
+
+export default router
