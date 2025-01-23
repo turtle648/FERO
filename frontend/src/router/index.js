7474ff4b9215daad0aeee3de87eafa638048673d
ungit003 <ungit003@gamil.com>
2025-01-23 12:17:20 +0900

    feat: 스타트-메인-로그인 라우팅

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StartPage from '@/views/StartPage.vue'
 import MainPage from '@/views/MainPage.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
 const routes = [
   {
@@ -11,13 +12,30 @@ const routes = [
   {
     path: '/main',
     name: 'Main',
-    component: MainPage
+    component: MainPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// 네비게이션 가드 설정
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
