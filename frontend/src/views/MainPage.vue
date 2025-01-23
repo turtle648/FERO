3984b80d94d88a4c9b6dea9f6fe20b3a91d74e7e
손채이 <im_1998@naver.com>
2025-01-23 15:25:34 +0900

    feat: 임시 달력 이미지 배치

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,32 +1,57 @@
 <template>
-  <div>
-    <img src="@/assets/backgroundImage.png" alt="배경이미지" />
+  <div class="main-container">
+    <div class="background-wrapper">
+      <img class="background-image" src="@/assets/images/backgroundImage.png" alt="배경이미지" />
+      <img class="calendar" src="@/assets/images/tmpImage.png" alt="임시달력" />
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
-div {
+.main-container {
   width: 100%;
   height: 100%;
-}
-img {
-  width: 100%;
-  height: 100%;
-  border: 1px solid black;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  position: relative;
 }
 
+.background-wrapper {
+  position: relative;
+  width: 27%;
+  height: 100%;
+}
+
+.background-image {
+  width: 100%;
+  height: 100%;
+}
+
+.calendar {
+  width: 30%;
+  position: absolute;
+  left: 0;
+  top: 20%;
+}
+
+/* 반응형 */
 @media screen and (max-width: 600px) {
-  img {
+  .background-wrapper {
     width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    height: auto;
+  }
+
+  .background-image {
+    width: 100%;
+    height: auto;
+  }
+
+  .calendar {
+    width: 25%;
+    top: 20%;
   }
 }
 </style>
