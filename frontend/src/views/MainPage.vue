338e7bc2b18a10ea8d81f3ec4349ce1674555c62
손채이 <im_1998@naver.com>
2025-01-23 14:15:55 +0900

    feat: 배경이미지 배치

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>
-        <h1>메인 페이지</h1>
-    </div>
+  <div>
+    <img src="@/assets/backgroundImage.png" alt="배경이미지" />
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
+div {
+  width: 100%;
+  height: 100%;
+}
+img {
+  width: 100%;
+  height: 100%;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+@media screen and (max-width: 600px) {
+  img {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
