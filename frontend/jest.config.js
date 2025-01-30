005c52944b8d66310479fda7d7bf682a8e70fc40
chaeyi0318 <chaeyi0318@gmail.com>
2025-01-31 02:27:17 +0900

    feat:테스트 코드 추가

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  testEnvironment: "jsdom", // 브라우저 환경 시뮬레이션
-  moduleFileExtensions: ["js", "json", "vue"], // 확장자 처리
+  testEnvironment: "jest-environment-jsdom", // 브라우저 환경 설정
+  moduleFileExtensions: ["js", "json", "vue"], // 처리할 파일 확장자
   transform: {
-    "^.+\\.vue$": "vue-jest", // .vue 파일 처리
-    "^.+\\.js$": "babel-jest", // .js 파일 처리
+    "^.+\\.vue$": "@vue/vue3-jest", // .vue 파일 변환
+    "^.+\\.js$": "babel-jest", // .js 파일 변환
   },
   transformIgnorePatterns: ["/node_modules/"], // node_modules 제외
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"], // Vue 3의 조건부 export 지원
+  },
 }
