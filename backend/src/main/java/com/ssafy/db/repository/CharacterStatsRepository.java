337b739ef5dd522d31982e9ec13f5c1cc5e4a74f
Kimdowon <dowon999@naver.com>
2025-02-04 14:26:22 +0900

    fix: CharacterStatsReporisory Error 해결

--- backend/src/main/java/com/ssafy/db/repository/CharacterStatsRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/CharacterStatsRepository.java
@@ -12,11 +12,11 @@ import java.util.Optional;
 @Repository
 public interface CharacterStatsRepository extends JpaRepository<UserStats, Long> {
     // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
-    Optional<User> findByUserId(String userId);
+//    Optional<User> findByUserId(String userId);
     // user_name 과 user_email 이 일치하는 회원 조회
-    Optional<User> findByUserNameAndUserEmail(String userName, String userEmail);
+//    Optional<User> findByUserNameAndUserEmail(String userName, String userEmail);
     // user_email 과 user_id 이 일치하는 회원 조회
-    Optional<User> findByUserEmailAndUserId(String userEmail, String userId);
+//    Optional<User> findByUserEmailAndUserId(String userEmail, String userId);
     // user_email 중복 확인
     boolean existsByUserEmail(String userEmail);
 }
