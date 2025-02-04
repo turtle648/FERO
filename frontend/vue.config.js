416065f5f0e978d6e0d473e9b2673d8744ade779
choheejin <hjcho9510@gmail.com>
2025-02-04 12:19:00 +0900

    hotfix(fronend): 개발 환경 분리중 에러 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,27 +1,26 @@
-// vue.config.js
-const fs = require('fs');
-const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
 
 const devConfig = {
   transpileDependencies: true,
-  devServer: {
+  devServer: process.env.NODE_ENV === 'development' ? {
     host: '0.0.0.0',
     allowedHosts: 'all',
-    https: {
-      key: fs.readFileSync(path.join(__dirname, 'certs/localhost.key')),
-      cert: fs.readFileSync(path.join(__dirname, 'certs/localhost.crt')),
-    },
+    https: (() => {
+      const fs = require('fs');
+      const path = require('path');
+      return {
+        key: fs.readFileSync(path.join(__dirname, 'certs/localhost.key')),
+        cert: fs.readFileSync(path.join(__dirname, 'certs/localhost.crt')),
+      };
+    })(),
     port: 5173,
-  },
+  } : {}
 };
 
 const prodConfig = {
   transpileDependencies: true,
-  // 프로덕션 환경 설정
 };
 
 module.exports = defineConfig(
   process.env.NODE_ENV === 'development' ? devConfig : prodConfig
 );
-
