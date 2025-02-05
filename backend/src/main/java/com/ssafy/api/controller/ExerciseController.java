ea12d415f43c8a05030d0cf5255df98ae2656f66
ansdudals8 <ansdudals8@naver.com>
2025-02-05 17:57:09 +0900

    feat : 사ì용자 운동 기록 조회 수정 필요

--- backend/src/main/java/com/ssafy/api/controller/ExerciseController.java
+++ backend/src/main/java/com/ssafy/api/controller/ExerciseController.java
@@ -1,6 +1,7 @@
 package com.ssafy.api.controller;
 
 import com.ssafy.api.request.ExerciseLogReq;
+import com.ssafy.api.request.ExerciseLogSearchReq;
 import com.ssafy.api.response.ExerciseLogRes;
 import com.ssafy.api.response.ExerciseStatsRatioRes;
 import com.ssafy.api.service.ExerciseLogService;
@@ -10,10 +11,15 @@ import com.ssafy.db.entity.ExerciseLog;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 사용자 운동에 대한 기록이나 보상에 대한 API.
@@ -49,4 +55,37 @@ public class ExerciseController {
 
         return ResponseEntity.ok(ExerciseLogRes.from(savedExerciseLog));
     }
+
+    @GetMapping("/logs")
+    @ApiOperation(value = "운동 기록 조회", notes = "사용자의 운동 기록을 다양한 조건으로 조회")
+    public ResponseEntity<List<ExerciseLogRes>> searchExerciseLogs(
+            HttpServletRequest request,
+            @RequestParam(required = false) Long exerciseStatsRatioId,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate searchDate
+    ) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || authHeader.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.emptyList());
+        }
+
+        try {
+            String userId = JwtTokenUtil.extractUserIdFromToken(authHeader);
+            System.out.println("조회 대상 사용자 ID : " + userId);
+
+            // DTO 대신 파라미터 직접 전달
+            ExerciseLogSearchReq searchOption = new ExerciseLogSearchReq();
+            searchOption.setExerciseStatsRatioId(exerciseStatsRatioId);
+            searchOption.setSearchDate(searchDate);
+
+            List<ExerciseLogRes> exerciseLogRes = exerciseLogService.searchExerciseLog(
+                    userId, searchOption
+            );
+            return ResponseEntity.ok(exerciseLogRes);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.emptyList());
+        }
+    }
+
 }
