602201a9338e0b76262981580a1515acbe488122
손채이 <im_1998@naver.com>
2025-02-06 10:22:30 +0900

    feat: 테일윈드

--- frontend/src/views/QRComponent.vue
+++ frontend/src/views/QRComponent.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="QR-BOX">
-        <h1>(나중에 큐알 들어갈 자리)</h1>
-    </div>
+  <div class="QR-BOX">
+    <h1>(나중에 큐알 들어갈 자리)</h1>
+  </div>
+  <div>
+    <h1 class="text-title">Hello Tailwind!</h1>
+  </div>
 </template>
 
 <script></script>
 
 <style>
 .QR-BOX {
-    border: solid;
-    border-color: black;
-    background-color: rgb(230, 255, 175);
+  border: solid;
+  border-color: black;
+  background-color: rgb(230, 255, 175);
+}
+
+.text-title {
+  @apply text-3xl font-bold underline text-center;
 }
 </style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // 템플릿 경로
+  darkMode: false, // 'media' 또는 'class'로 변경 가능
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
