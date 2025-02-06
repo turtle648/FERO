dc71d543560d1e4ea6d22d75d1ea6c89b98a9d03
손채이 <im_1998@naver.com>
2025-02-06 12:06:38 +0900

    feat: 테일윈드 설정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // 템플릿 경로
-  darkMode: false, // 'media' 또는 'class'로 변경 가능
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
