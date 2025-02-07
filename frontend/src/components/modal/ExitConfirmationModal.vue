d580870930bf6d3d0cdf66dd5722eb9939c8deb8
손채이 <im_1998@naver.com>
2025-02-07 12:09:46 +0900

    feat: 종료, 신고 버튼

---
+++ frontend/src/components/modal/ExitConfirmationModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    종료ㄱㄱ?
+    <ExitButton />
+  </div>
+</template>
+
+<script setup>
+import ExitButton from "@/assets/button/ExitButton.vue"
+</script>
+
+<style scoped></style>
