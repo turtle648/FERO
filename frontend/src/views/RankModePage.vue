573d3d3676213e7ecb5f3495d9ae9b3751da0ef3
Jinseong <pnum4095@gmail.com>
2025-02-10 10:18:06 +0900

    feat: 빌드를 위한 주석 처리

--- frontend/src/views/RankModePage.vue
+++ frontend/src/views/RankModePage.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script setup>
-import { defineProps, ref } from 'vue'
+// import { defineProps, ref } from 'vue'
 
-defineProps({
-  exercise: {
-    type: String,
-    required: true
-  }
-})
+// defineProps({
+//   exercise: {
+//     type: String,
+//     required: true
+//   }
+// })
 
-const defaultCount = ref(2)
+// const defaultCount = ref(2)
 </script>
 
 <style scoped>
