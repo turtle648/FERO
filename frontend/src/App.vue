bf1e62e7bbe0f91dea6f79dbc34ef98918c54393
Jinseong <pnum4095@gmail.com>
2025-02-10 10:41:45 +0900

    feat: 모바일 QR테스트

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,9 @@
 <template>
-  <router-view v-if="!isDesktop" />
-  <QRComponent v-else />
+  <router-view />
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue"
-import QRComponent from "@/components/QRComponent.vue"
 
-// matchMedia()를 사용하여 초기값 설정
-const mediaQuery = window.matchMedia("(min-width: 821px)")
-const isDesktop = ref(mediaQuery.matches)
-
-// 반응형 감지를 위한 핸들러 함수
-const checkScreenSize = (event) => {
-  isDesktop.value = event.matches
-}
-
-onMounted(() => {
-  // matchMedia 이벤트 리스너 사용 (resize 대신)
-  mediaQuery.addEventListener("change", checkScreenSize)
-})
-
-onUnmounted(() => {
-  mediaQuery.removeEventListener("change", checkScreenSize)
-})
 </script>
 
 <style scoped></style>
