a1cfe92c003b53d1dad264963efacadc70e4aab0
손채이 <im_1998@naver.com>
2025-02-10 10:50:03 +0900

    fix: 라우터 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <router-view v-if="!isDesktop" />
-  <QRComponent v-else />
+  <!-- <router-view v-if="!isDesktop" />
+  <QRComponent v-else /> -->
+  <router-view />
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue"
-import QRComponent from "@/components/QRComponent.vue"
+// import { ref, onMounted, onUnmounted } from "vue"
+// import QRComponent from "@/components/QRComponent.vue"
 
-const isDesktop = ref(window.matchMedia("(min-width: 821px)").matches)
-const checkScreenSize = () => {
-  isDesktop.value = window.innerWidth > 821
-}
+// const isDesktop = ref(window.matchMedia("(min-width: 821px)").matches)
+// const checkScreenSize = () => {
+//   isDesktop.value = window.innerWidth > 821
+// }
 
-onMounted(() => {
-  window.addEventListener("resize", checkScreenSize)
-})
+// onMounted(() => {
+//   window.addEventListener("resize", checkScreenSize)
+// })
 
-onUnmounted(() => {
-  window.removeEventListener("resize", checkScreenSize)
-})
+// onUnmounted(() => {
+//   window.removeEventListener("resize", checkScreenSize)
+// })
 </script>
 
 <style scoped></style>
