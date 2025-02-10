acb88aeef4689ba61ed1a48d7a7c8d34db80ee21
Jinseong <pnum4095@gmail.com>
2025-02-10 10:44:46 +0900

    feat: App.vue 기존 값 복원

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <router-view />
+  <router-view v-if="!isDesktop" />
+  <QRComponent v-else />
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from "vue"
+import QRComponent from "@/components/QRComponent.vue"
 
+const isDesktop = ref(window.matchMedia("(min-width: 821px)").matches)
+const checkScreenSize = () => {
+  isDesktop.value = window.innerWidth > 821
+}
+
+onMounted(() => {
+  window.addEventListener("resize", checkScreenSize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener("resize", checkScreenSize)
+})
 </script>
 
 <style scoped></style>
