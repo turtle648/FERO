d4c84d923d0618bc08e76802cd40af7fdc8c49bc
손채이 <im_1998@naver.com>
2025-02-13 10:35:53 +0900

    fix: 미디어파이프 수정

--- frontend/src/components/MediapipeComponent.vue
+++ frontend/src/components/MediapipeComponent.vue
@@ -11,7 +11,7 @@
     <!-- 본인 화면 -->
     <div class="relative w-full h-full flex justify-center items-center overflow-hidden">
       <canvas ref="canvasElement" class="">
-        <video ref="videoElement" class="aspect-[9/16] w-auto h-full"></video>
+        <video ref="videoElement" class="aspect-[9/16] w-full h-auto object-cover"></video>
         <!-- fix: 비디오에만 태그 달기기, 화면에 표시될 크기 지정 -->
         <!-- 미디어파이프 표시되는 화면이 가로로 긴 화면인데 세로를 풀 중심점을 화면 표시되는 중심으로 잡아서 중심에서 9: 16 맞추기 (나머지 부분 잘리고 스크롤 없이) -->
       </canvas>
