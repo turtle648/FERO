95864fced4381302abd589aa2ec737c698d4498c
chaeyi0318 <chaeyi0318@gmail.com>
2025-02-18 00:51:40 +0900

    feat: 메인페이지 튜토리얼 모달

--- frontend/src/components/modal/TutorialConfirmModal.vue
+++ frontend/src/components/modal/TutorialConfirmModal.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>rr? ss?</div>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div class="bg-white w-[40vh] p-6 rounded-lg shadow-lg">
+      <p class="text-lg mb-4 font-dgm">
+        메인페이지에 처음 입장하셨다면,
+        <br />
+        페이지에 대해서 안내해드릴까요?
+      </p>
+      <div class="flex justify-center gap-4">
+        <button class="font-dgm nes-btn is-primary" @click="$emit('accept')">수락</button>
+        <button class="font-dgm nes-btn is-error" @click="$emit('reject')">거절</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineEmits } from "vue"
 
-<style lang="scss" scoped></style>
+defineEmits(["accept", "reject"])
+</script>
