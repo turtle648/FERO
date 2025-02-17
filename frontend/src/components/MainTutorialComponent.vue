08b2ba7b01d717aae824fbf8677f832992e6d6fd
chaeyi0318 <chaeyi0318@gmail.com>
2025-02-18 00:40:49 +0900

    feat: 메인페이지 튜토리얼

---
+++ frontend/src/components/MainTutorialComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <img src="@/assets/images/pesocom.png" alt="" />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/modal/TutorialConfirmModal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>rr? ss?</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
