bf869c484bcedfac5c250a50613b9d08486f0846
손채이 <im_1998@naver.com>
2025-02-18 10:27:15 +0900

    feat: 메인 페이지 튜토리얼 완료

--- frontend/src/components/MainTutorialComponent.vue
+++ frontend/src/components/MainTutorialComponent.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="img-container z-50 fixed inset-0 flex items-center justify-center" @click="nextStep">
+  <div class="img-container z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" @click="nextStep">
     <!-- 설명 단계별로 표시 -->
     <section class="message-list flex items-center absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <!-- 이미지 -->
       <img src="@/assets/images/pesocom.png" class="w-[15vh] object-contain mt-40" alt="Tutorial Image" />
 
       <!-- Balloon -->
-      <div class="desc-container nes-balloon from-left ml-4 w-[37vh] h-[12vh] -translate-y-4">
-        <span v-if="currentStep === 1" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">ㅎㅇㅎㅇ 저는 파소콩</span>
-        <span v-if="currentStep === 2" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">먼저 왼쪽 상단에는 타이머가 있어</span>
-        <span v-if="currentStep === 3" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">운동 시간을 체크할 수 있음</span>
-        <span v-if="currentStep === 4" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">그 옆 오른쪽에는 카운트!</span>
-        <span v-if="currentStep === 5" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">바른 자세로 운동을 하면 카운트가 +1</span>
-        <span v-if="currentStep === 6" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">스쿼트를 할 때 자세에 대해 알려드림</span>
-        <span v-if="currentStep === 7" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">카메라를 약간 측면으로 보고 서서</span>
-        <span v-if="currentStep === 8" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">무릎이 90도가 될 때까지</span>
-        <span v-if="currentStep === 9" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">내려갔다가 올라오면 카운트가 + 1</span>
-        <span v-if="currentStep === 10" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">아래의 가운데에는 종료 버튼이 있음</span>
-        <span v-if="currentStep === 11" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">운동을 하다가 종료하고 싶을 때 클릭!</span>
-        <span v-if="currentStep === 12" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">종료 버튼 옆에는 신고 버튼이 있음</span>
-        <span v-if="currentStep === 13" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">운동을 하다가 버그가 발생했거나</span>
-        <span v-if="currentStep === 14" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">불쾌한 옷차림의 유저를 만났을 때 신고하면 됨</span>
-        <span v-if="currentStep === 15" class="font-dgm sm:text-base md:text-lg lg:text-xl xl:text-2xl">자 그럼 바로 운동을 하러 가볼까?</span>
+      <div class="desc-container nes-balloon from-left ml-4 w-[39vh] h-[13vh] -translate-y-1 flex justify-center items-center">
+        <span v-if="currentStep === 1" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">안녕하세요 용사님</span>
+        <span v-if="currentStep === 2" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">메인페이지 기능에 대해 설명해드릴게요</span>
+        <span v-if="currentStep === 3" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">상단의 유저정보를 클릭하면 스탯을 확인할 수 있습니다</span>
+        <span v-if="currentStep === 4" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">메인의 중앙에 있는 캐릭터를 클릭하면</span>
+        <span v-if="currentStep === 5" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">캐릭터의 얼굴, 옷, 헤어스타일을 바꿀 수 있습니다</span>
+        <span v-if="currentStep === 6" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">이제 하단 아이콘에 대해 설명 드리겠습니다</span>
+        <span v-if="currentStep === 7" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">가장 왼쪽의 Calendar를 클릭하면</span>
+        <span v-if="currentStep === 8" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">퀘스트 달성 여부와 스탯 성장률을 볼 수 있습니다</span>
+        <span v-if="currentStep === 9" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">Record를 클릭하면 유저의 전적 기록을 확인할 수 있습니다</span>
+        <span v-if="currentStep === 10" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">가운데 Fitness 버튼은 운동을 할 때 사용할 수 있습니다</span>
+        <span v-if="currentStep === 11" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">하고 싶은 종목과 모드를 선택하여 운동을 할 수 있습니다</span>
+        <span v-if="currentStep === 12" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">모드는 혼자 운동을 할 수 있는 싱글 모드</span>
+        <span v-if="currentStep === 13" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">랜덤 매칭을 하고 랭크를 올릴 수 있는 랭크모드가 있습니다</span>
+        <span v-if="currentStep === 14" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">Quest 버튼은 매일 퀘스트를 확인할 수 있습니다</span>
+        <span v-if="currentStep === 15" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">그리고 마지막으로 Setting 버튼에서는 회원탈퇴와</span>
+        <span v-if="currentStep === 16" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">음악 볼륨 조절을 할 수 있습니다</span>
+        <span v-if="currentStep === 17" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">자 그럼 바로 운동을 하러 가볼까요?</span>
       </div>
     </section>
 
     <!-- 튜토리얼 완료 버튼 -->
-    <div v-if="currentStep === 15" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-4 z-50">
+    <div v-if="currentStep === 17" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-4 z-50">
       <button class="px-4 py-2 nes-btn is-primary font-dgm z-50" @click="completeUiTutorial">튜토리얼 완료!</button>
     </div>
   </div>
@@ -45,7 +47,7 @@ const currentStep = ref(1)
 
 // 다음 단계로 이동하는 함수
 const nextStep = () => {
-  if (currentStep.value < 15) {
+  if (currentStep.value < 17) {
     currentStep.value += 1 // 다음 단계로 이동
   }
 }
