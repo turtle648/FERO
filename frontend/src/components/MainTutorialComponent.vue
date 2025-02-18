42794db708070c8966d5cd2d59edeb66509506b9
손채이 <im_1998@naver.com>
2025-02-18 10:34:38 +0900

    feat: 글자색 변경

--- frontend/src/components/MainTutorialComponent.vue
+++ frontend/src/components/MainTutorialComponent.vue
@@ -9,19 +9,57 @@
       <div class="desc-container nes-balloon from-left ml-4 w-[39vh] h-[13vh] -translate-y-1 flex justify-center items-center">
         <span v-if="currentStep === 1" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">안녕하세요 용사님</span>
         <span v-if="currentStep === 2" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">메인페이지 기능에 대해 설명해드릴게요</span>
-        <span v-if="currentStep === 3" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">상단의 유저정보를 클릭하면 스탯을 확인할 수 있습니다</span>
-        <span v-if="currentStep === 4" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">메인의 중앙에 있는 캐릭터를 클릭하면</span>
+        <span v-if="currentStep === 3" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          상단의
+          <span class="text-red-500">유저정보</span>
+          를 클릭하면 스탯을 확인할 수 있습니다
+        </span>
+        <span v-if="currentStep === 4" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          메인의 중앙에 있는
+          <span class="text-red-500">캐릭터를 클릭</span>
+          하면
+        </span>
         <span v-if="currentStep === 5" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">캐릭터의 얼굴, 옷, 헤어스타일을 바꿀 수 있습니다</span>
-        <span v-if="currentStep === 6" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">이제 하단 아이콘에 대해 설명 드리겠습니다</span>
-        <span v-if="currentStep === 7" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">가장 왼쪽의 Calendar를 클릭하면</span>
+        <span v-if="currentStep === 6" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          이제
+          <span class="text-red-500">하단 아이콘</span>
+          에 대해 설명 드리겠습니다
+        </span>
+        <span v-if="currentStep === 7" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          가장 왼쪽의
+          <span class="text-red-500">Calendar</span>
+          를 클릭하면
+        </span>
         <span v-if="currentStep === 8" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">퀘스트 달성 여부와 스탯 성장률을 볼 수 있습니다</span>
-        <span v-if="currentStep === 9" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">Record를 클릭하면 유저의 전적 기록을 확인할 수 있습니다</span>
-        <span v-if="currentStep === 10" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">가운데 Fitness 버튼은 운동을 할 때 사용할 수 있습니다</span>
+        <span v-if="currentStep === 9" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          <span class="text-red-500"></span>
+          Record를 클릭하면 유저의 전적 기록을 확인할 수 있습니다
+        </span>
+        <span v-if="currentStep === 10" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          가운데
+          <span class="text-red-500"></span>
+          Fitness 버튼은 운동을 할 때 사용할 수 있습니다
+        </span>
         <span v-if="currentStep === 11" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">하고 싶은 종목과 모드를 선택하여 운동을 할 수 있습니다</span>
-        <span v-if="currentStep === 12" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">모드는 혼자 운동을 할 수 있는 싱글 모드</span>
-        <span v-if="currentStep === 13" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">랜덤 매칭을 하고 랭크를 올릴 수 있는 랭크모드가 있습니다</span>
-        <span v-if="currentStep === 14" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">Quest 버튼은 매일 퀘스트를 확인할 수 있습니다</span>
-        <span v-if="currentStep === 15" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">그리고 마지막으로 Setting 버튼에서는 회원탈퇴와</span>
+        <span v-if="currentStep === 12" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          모드는 혼자 운동을 할 수 있는
+          <span class="text-red-500"></span>
+          싱글 모드
+        </span>
+        <span v-if="currentStep === 13" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          랜덤 매칭을 하고 랭크를 올릴 수 있는
+          <span class="text-red-500">랭크모드</span>
+          가 있습니다
+        </span>
+        <span v-if="currentStep === 14" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          <span class="text-red-500">Quest 버튼</span>
+          은 매일 퀘스트를 확인할 수 있습니다
+        </span>
+        <span v-if="currentStep === 15" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">
+          그리고 마지막으로
+          <span class="text-red-500">Setting 버튼</span>
+          에서는 회원탈퇴와
+        </span>
         <span v-if="currentStep === 16" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">음악 볼륨 조절을 할 수 있습니다</span>
         <span v-if="currentStep === 17" class="font-dgm sm:text-xs md:text-lg lg:text-xl xl:text-2xl">자 그럼 바로 운동을 하러 가볼까요?</span>
       </div>
