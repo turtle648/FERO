6366f75c256c23b68bfcdeadf2467a617f638da0
조희진 <allana123@naver.com>
2025-02-18 17:40:35 +0900

    fix(front): 결과 성공/실패 로직 에러 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -20,11 +20,11 @@ const devConfig = {
       : {
           host: "0.0.0.0",
           allowedHosts: "all",
-          // https: {
-          //   key: fs.readFileSync(path.join(__dirname, "certs/localhost.key")),
-          //   cert: fs.readFileSync(path.join(__dirname, "certs/localhost.crt")),
-          // },
-          https: false,
+          https: {
+            key: fs.readFileSync(path.join(__dirname, "certs/localhost.key")),
+            cert: fs.readFileSync(path.join(__dirname, "certs/localhost.crt")),
+          },
+          // https: false,
           port: 5173,
         },
   pwa: {
